1228f7ad13d6fa9425477e97f524f185336d9d8a
Alekbac <aleksandra.bachanek1@gmail.com>
2019-04-03 21:11:39 +0200

    Second Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public void addition(){
+
+    }
+    public void substraction(){
+
+    }
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        calculator.addition();
+        calculator.substraction();
+        System.out.println("It works!");
+    }
+}

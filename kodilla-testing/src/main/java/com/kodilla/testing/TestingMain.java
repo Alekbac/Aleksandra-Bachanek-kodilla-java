61d8f499a5b53a963a30b1e07c683023326c59e4
Alekbac <aleksandra.bachanek1@gmail.com>
2019-04-12 15:57:13 +0200

    Modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        Calculator calc = new Calculator();
+
+        int expectSum = 50;
+        int actualSum = calc.add(a, b);
+
+        int expectDifference = -10;
+        int actualDifference = calc.substract( a, b);
+
+        if (actualSum != expectSum) {
+            System.out.println("Error");
+        } else System.out.println("Result of sum is OK");
+
+        if (expectDifference != actualDifference) {
+            System.out.println("Error");
+        } else System.out.println("Result of sum is OK");
     }
 }

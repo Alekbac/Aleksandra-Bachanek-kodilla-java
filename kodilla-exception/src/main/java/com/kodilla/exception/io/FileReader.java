f288792a0a33d7f3ad968bf9b0b0d34fd48a8620
Alekbac <aleksandra.bachanek1@gmail.com>
2019-04-28 18:47:20 +0200

    Moduł 8.1, 8.2, przykład

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -7,16 +7,16 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile(throws FileReaderException){
+    public void readFile()throws FileReaderException{
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource( "file/names.txt" ).getFile());
 
         try(Stream<String> fileLanes = Files.lines(Paths.get(file.getPath()))){
-            fileLanes.forEach( (System.out::println) );
+            fileLanes.forEach( System.out::println );
 
         } catch (IOException e){
             throw new FileReaderException();
-            System.out.println("Oh noooo!"+e);
+            //System.out.println("Oh noooo!"+e);
 
         }finally{
             System.out.println("Will be fine.");

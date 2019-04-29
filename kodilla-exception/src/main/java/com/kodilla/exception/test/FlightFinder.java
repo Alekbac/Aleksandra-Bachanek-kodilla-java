6a7651e4a2e6be2281f8b804e3ab1824174c3a9e
Alekbac <aleksandra.bachanek1@gmail.com>
2019-04-29 17:53:03 +0200

    Moduł 8.4, zadanie

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,60 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+
+    public boolean findFlight(Flight flight) throws RouteNotFoundException{
+        Map<String, Boolean> flyingDirection = new HashMap<>();
+        flyingDirection.put( "Chicago", true );
+        flyingDirection.put( "New York", true );
+        flyingDirection.put( "Dallas", false );
+        flyingDirection.put( "Los Angeles", false );
+
+        String departure = flight.getDepartureAirport();
+        String arrival = flight.getArrivalAirport();
+
+        if(flyingDirection.containsKey(arrival) && (flyingDirection.containsKey(departure))){
+            return (flyingDirection.get(arrival) && flyingDirection.get(departure));
+        }else {
+            String report;
+
+            if (!flyingDirection.containsKey( arrival )) {
+                report = "The plane form " + departure + " don't land in " + arrival;
+            } else if (!flyingDirection.containsKey( departure )) {
+                report = "The plane to " + arrival + " don't fly from " + departure;
+            } else {
+                report = "There isn't connection between" + departure + " and " + arrival;
+            }
+            throw new RouteNotFoundException(report);
+        }
+    }
+
+    public static void main(String[] args) {
+        Flight flight1 = new Flight( "Chicago", "Buenos Aires" );
+        Flight flight2 = new Flight( "London", "Los Angeles" );
+        Flight flight3 = new Flight( "Warsaw", "Berlin" );
+        //We flight 3 chciałam uzyskać komunikat "There isn't connection between" + departure + " and " + arrival;"
+
+        FlightFinder flightFinder = new FlightFinder();
+
+        try {
+            System.out.println( flightFinder.findFlight( flight1 ) );
+        } catch (RouteNotFoundException e) {
+            System.out.println( e );
+        }
+
+        try {
+            System.out.println( flightFinder.findFlight( flight2 ) );
+        } catch (RouteNotFoundException e) {
+            System.out.println( e );
+        }
+
+        try {
+            System.out.println( flightFinder.findFlight( flight3 ) );
+        } catch (RouteNotFoundException e) {
+            System.out.println( e );
+        }
+    }
+}

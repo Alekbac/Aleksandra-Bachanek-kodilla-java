363318d8d9724bff1693673948f678976aa3e932
Alekbac <aleksandra.bachanek1@gmail.com>
2019-05-15 16:51:37 +0200

    Moduł 10.3, przykład

--- kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
@@ -1,9 +1,14 @@
 package com.kodilla.spring.reader;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ReaderTestSuite {
     @Test
     public void testRead() {

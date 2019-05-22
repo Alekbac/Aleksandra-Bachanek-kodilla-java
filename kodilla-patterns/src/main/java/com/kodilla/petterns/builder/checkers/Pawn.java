6066fc6024a35d22be176e737ec8a3680545064d
Alekbac <aleksandra.bachanek1@gmail.com>
2019-05-22 21:05:52 +0200

    Moduł 11.5, przykład

---
+++ kodilla-patterns/src/main/java/com/kodilla/petterns/builder/checkers/Pawn.java
@@ -0,0 +1,7 @@
+package com.kodilla.petterns.builder.checkers;
+
+public final class Pawn extends Figure {
+    public Pawn(String color) {
+        super(color);
+    }
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/petterns/builder/checkers/Queen.java
@@ -0,0 +1,7 @@
+package com.kodilla.petterns.builder.checkers;
+
+public class Queen extends Figure {
+    public Queen(String color) {
+        super(color);
+    }
+}

57d2a08a2001fd9177c9990617d65623380c0427
infinite-dawn-60500 <aleksandra.bachanek1@gmail.com>
2019-08-26 13:36:37 +0200

    Moduł 20.4, przykład

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/forum/JavaToolsForumTopic.java
@@ -0,0 +1,10 @@
+package com.kodilla.patterns2.observer.forum;
+
+import sun.font.FontRunIterator;
+
+public class JavaToolsForumTopic extends ForumTopic {
+    public JavaToolsForumTopic(){
+        super("Java Tools Group");
+    }
+
+}
